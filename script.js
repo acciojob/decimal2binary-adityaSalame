34f764eae69baadb3587bbc4e48f9da08ede516b
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-06-24 20:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	
+
+// to convert positive decimal to binary
+let binary = arr.toString( redix );
+
+// to convert negative decimal to binary
+
+  return binary;
 }
 
 module.exports = threeSum;
