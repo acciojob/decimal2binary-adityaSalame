15a9be9ba32d27b5e90d18a7ab45e3c61b5ac6bb
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:08:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-function threeSum(arr, target) {
-// write your code here
-	
-
-// to convert positive decimal to binary
-let binary = arr.toString( redix );
-
-// to convert negative decimal to binary
-
-  return binary;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-module.exports = threeSum;
+window.decimalToBinary = decimalToBinary;
